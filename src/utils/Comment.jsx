d94e1ebed42562c0c8273d5a1502c4252706f7c8
sabiq <salnafsabiq@gmail.com>
2024-09-18 05:37:18 +0700

    testing

--- src/utils/Comment.jsx
+++ src/utils/Comment.jsx
@@ -1,19 +1,17 @@
 import { Row, Col, Button } from "react-bootstrap";
 import React, { useEffect } from "react";
-import { Comments } from "@hyvor/hyvor-talk-react";
-import { CommentCount } from "@hyvor/hyvor-talk-react";
+import { Comments, CommentCount } from "@hyvor/hyvor-talk-react";
 import { CommentCounts } from "@hyvor/hyvor-talk-base";
 
-const Coment = ({ article }) => {
+const Coment = () => {
   useEffect(() => {
     CommentCounts.load({
       "website-id": "11900",
     });
   }, []);
-  const src = "/gambar/information.png";
 
   return (
-    <Row
+    <div
       style={{
         textAlign: "center",
         boxSizing: "border-box",
@@ -24,45 +22,20 @@ const Coment = ({ article }) => {
         height: "100vh",
       }}
     >
-      {/* <Col md={6} className="mx-auto">
-        <h2
-          style={{
-            marginBottom: "2rem",
-            marginTop: "2rem",
-            fontFamily: "Dancing Script",
-            color: "#e63946",
-            fontSize: "3rem",
-          }}
-        >
-          Ucapan
-        </h2>
-        <div
-          className="coment-form"
-          style={{
-            justifyContent: "center",
-            alignItems: "center",
-            textAlign: "center",
-            padding: "1rem 1rem 1rem 1rem",
-            boxSizing: "border-box",
-            width: "350px",
-            height: "auto",
-          }}
-        >
-          <Comments
-            website-id={11900} // Ganti dengan ID website dari Hyvor Talk
-            page-id="unique-page-1" // Ganti dengan ID unik untuk halaman ini
-          />
-          <div>
-            <p>
-              Halaman 1: <CommentCount page-id="page-1" />
-            </p>
-            <p>
-              Halaman 2: <CommentCount page-id="page-2" />
-            </p>
-          </div>
-        </div>
-      </Col> */}
-    </Row>
+      <div>
+        <h1>Halaman Komentar</h1>
+        {/* Komponen Comments untuk menampilkan komentar */}
+        <Comments
+          website-id="11900" // ID website dari Hyvor Talk Anda
+          page-id="page-1" // ID halaman unik yang bisa Anda tentukan sendiri
+        />
+
+        {/* Komponen CommentCount untuk menampilkan jumlah komentar */}
+        <p>
+          Jumlah Komentar: <CommentCount />
+        </p>
+      </div>
+    </div>
   );
 };
 export default Coment;
