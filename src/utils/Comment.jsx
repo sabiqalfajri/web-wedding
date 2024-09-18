70570e0ade57f19a8a6c5649ab5b1f76f5e8548d
sabiq <salnafsabiq@gmail.com>
2024-09-19 02:14:21 +0700

    testing

--- src/utils/Comment.jsx
+++ src/utils/Comment.jsx
@@ -6,7 +6,7 @@ const Coment = ({ url }) => {
   const { name } = useParams();
 
   // Membuat URL dinamis berdasarkan nama tamu
-  const currentPageUrl = `https://digital.com/mail&asih/${name}`;
+
   const src = "/gambar/information.png";
   return (
     <div
@@ -19,22 +19,7 @@ const Coment = ({ url }) => {
         backgroundSize: "contain",
         height: "100vh",
       }}
-    >
-      <div id="fb-root"></div>
-      <script
-        async
-        defer
-        crossorigin="anonymous"
-        src="https://connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v20.0"
-        nonce="sEGzHr1U"
-      ></script>
-      <div
-        className="fb-comments"
-        data-href={currentPageUrl}
-        data-width="100%"
-        data-numposts="5"
-      ></div>
-    </div>
+    ></div>
   );
 };
 export default Coment;
