b65ed27c6ae93e11466151a8c1e0c7ffe2592136
sabiq <salnafsabiq@gmail.com>
2024-09-19 02:46:09 +0700

    testing

--- src/utils/Closing.jsx
+++ src/utils/Closing.jsx
@@ -12,7 +12,6 @@ const Closing = () => {
           flexDirection: "column",
           width: "100%",
           display: "flex",
-          flexDirection: "column",
           boxSizing: "border-box",
         }}
       >
