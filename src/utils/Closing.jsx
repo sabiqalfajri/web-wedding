b504193ee7b1765a51cd7e3995c6ad80d468da49
sabiq <salnafsabiq@gmail.com>
2024-09-19 10:50:13 +0700

    terbarunich

--- src/utils/Closing.jsx
+++ src/utils/Closing.jsx
@@ -1,71 +1,68 @@
-import ParticlesComponent from "./Effect";
-import React from "react";
-
 const Closing = () => {
   return (
     <>
       <div
         className="closing"
-        style={{ position: "relative", width: "100%", height: "100vh" }}
+        style={{
+          position: "relative",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          height: "100vh",
+          textAlign: "center",
+          overflow: "hidden",
+        }}
       >
-        <ParticlesComponent />
         <div
-          className="closing-content"
+          className="snnow"
           style={{
-            position: "relative",
             zIndex: 1,
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            justifyContent: "center",
-            textAlign: "center",
+          }}
+        ></div>
+        <img
+          src="/gambar/border-slideterakhir.png"
+          alt=""
+          style={{
+            maxWidth: "25rem",
+            height: "auto",
+          }}
+        />
+        <div
+          style={{
+            maxWidth: "25rem",
           }}
         >
-          <img
-            src="/gambar/border-slideterakhir.png"
-            alt=""
+          <p
             style={{
-              maxWidth: "25rem",
-              height: "auto",
-              zIndex: 1,
-            }}
-          />
-          <div
-            style={{
-              maxWidth: "25rem",
-              zIndex: 1,
+              marginTop: "-8rem",
+              color: "white",
             }}
           >
-            <p
-              style={{
-                marginTop: "-8rem",
-              }}
-            >
-              Menjadi suatu kebahagiaan bagi kami apabila Bapak/Ibu/Saudara/i
-              berkenan hadir dalam hari bahagia ini. Terima kasih atas segala
-              ucapan, doa, dan perhatian yang diberikan
-            </p>
-          </div>
-          <div
+            Menjadi suatu kebahagiaan bagi kami apabila Bapak/Ibu/Saudara/i
+            berkenan hadir dalam hari bahagia ini. Terima kasih atas segala
+            ucapan, doa, dan perhatian yang diberikan
+          </p>
+        </div>
+        <div
+          style={{
+            maxWidth: "20rem",
+            color: "white",
+          }}
+        >
+          {" "}
+          <strong>See you on your wedding day!</strong>
+          <strong
             style={{
-              maxWidth: "20rem",
-              zIndex: 1,
+              fontSize: "1.2rem",
+              fontFamily: "Dancing Script",
+              marginTop: "1rem",
+              display: "flex",
+              flexDirection: "column",
             }}
           >
-            {" "}
-            <strong>See you on your wedding day!</strong>
-            <strong
-              style={{
-                fontSize: "1.2rem",
-                fontFamily: "Dancing Script",
-                marginTop: "1rem",
-                display: "flex",
-                flexDirection: "column",
-              }}
-            >
-              Mail & Asih
-            </strong>
-          </div>
+            Mail & Asih
+          </strong>
         </div>
       </div>
     </>
