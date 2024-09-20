daa987e05e5c3c50a46b58859b6312abce25cad2
sabiq <salnafsabiq@gmail.com>
2024-09-21 03:14:24 +0700

    fakk

--- src/utils/Closing.jsx
+++ src/utils/Closing.jsx
@@ -20,7 +20,7 @@ const Closing = () => {
         ease: "power1.inOut",
         scrollTrigger: {
           trigger: image,
-          start: "top center%", // Mulai animasi saat bagian atas gambar mencapai tengah layar
+          start: "top center", // Mulai animasi saat bagian atas gambar mencapai tengah layar
           end: "bottom top", // Akhir animasi saat bawah gambar mencapai tengah layar
           scrub: true, // Animasi mengikuti scroll
           invalidateOnRefresh: true,
