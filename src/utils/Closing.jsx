0a5ae2b71b85013ae8aac6bc28ddea7fdb987a87
sabiq <salnafsabiq@gmail.com>
2024-09-22 07:42:37 +0700

    testt

--- src/utils/Closing.jsx
+++ src/utils/Closing.jsx
@@ -1,5 +1,6 @@
 import React, { useRef } from "react";
 import { motion, useScroll } from "framer-motion";
+import { useTransform } from "framer-motion";
 
 const Closing = () => {
   const ref = useRef(null);
@@ -7,27 +8,26 @@ const Closing = () => {
     target: ref,
     offset: ["0 1", "1 0"], // Mengatur offset untuk zoom
   });
+  const backroundSize = useTransform(scrollYProgress, [0, 1], ["10%", "200%"]);
 
   return (
     <div className="wrapper" ref={ref}>
-      <motion.div
-        className="image-container"
-        style={{
-          scale: scrollYProgress, // Zoom hingga memenuhi layar (2x)
-          opacity: 1 - scrollYProgress, // Opacity dari 1 ke 0
-          originX: 0.5,
-          originY: 0.5,
-        }}
-      >
-        <img
-          src="/gambar/border-slideterakhir.png" // Ganti dengan URL gambar Anda
-          alt="Zoom"
-          className="zoom-image"
-        />
-      </motion.div>
-      {/* <section className="content">
-        <div className="text">Haloo brow</div>
-      </section> */}
+      <div className="content">
+        <motion.section
+          className="section hero"
+          style={{
+            scale: backroundSize, // Zoom hingga memenuhi layar (2x)
+            opacity: 1 - scrollYProgress, // Opacity dari 1 ke 0
+            // originX: 0.5,
+            // originY: 0.5,
+          }}
+        ></motion.section>
+        <section className="section gradient-purple">section 2</section>
+        <section className="section gradient-blue">section 3</section>
+        <div className="image-container">
+          <img src="" alt="" />
+        </div>
+      </div>
     </div>
   );
 };
