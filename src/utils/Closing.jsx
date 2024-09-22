e2172c73e07ffa3f5bdf8cdae6b7ddf6ad153de8
sabiq <salnafsabiq@gmail.com>
2024-09-22 08:07:55 +0700

    testing

--- src/utils/Closing.jsx
+++ src/utils/Closing.jsx
@@ -6,10 +6,14 @@ const Closing = () => {
   const ref = useRef(null);
   const { scrollYProgress } = useScroll({
     target: ref,
-    offset: ["0 1", "1 0"], // Mengatur offset untuk zoom
+    offset: ["start start", "end start"], // Mengatur offset untuk zoom
   });
-  const backroundSize = useTransform(scrollYProgress, [0, 1], ["10%", "200%"]);
-
+  const backroundSize = useTransform(
+    scrollYProgress,
+    [0, 0.8],
+    ["10%", "200%"]
+  );
+  const opacitiy = useTransform(scrollYProgress, [0, 0.8], [1, 0]);
   return (
     <div className="wrapper" ref={ref}>
       <div className="content">
@@ -17,12 +21,17 @@ const Closing = () => {
           className="section hero"
           style={{
             scale: backroundSize, // Zoom hingga memenuhi layar (2x)
-            opacity: 1 - scrollYProgress, // Opacity dari 1 ke 0
             // originX: 0.5,
             // originY: 0.5,
           }}
         ></motion.section>
-        <section className="section gradient-purple">section 2</section>
+        <motion.section
+          className="section gradient-purple"
+          initial={{ opacitiy: 0 }}
+          animate={{ opacity: scrollYProgress >= 0.8 ? 1 : 0 }}
+        >
+          section 2
+        </motion.section>
         <section className="section gradient-blue">section 3</section>
         <div className="image-container">
           <img src="" alt="" />
