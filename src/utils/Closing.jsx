99b5f857dfe659842fb9e31df5b5ac1650917f17
sabiq <salnafsabiq@gmail.com>
2024-09-26 06:52:11 +0700

    testing

--- src/utils/Closing.jsx
+++ src/utils/Closing.jsx
@@ -60,155 +60,173 @@ const Closing = () => {
   }, [isTyping]);
 
   return (
-    <div className="wrapper">
-      <div className="content">
-        <section className="section hero">
-          <div
-            style={{
-              justifyContent: "center",
-              textAlign: "center",
-            }}
-          >
+    <>
+      {/* <div className="wrapper">
+        <div className="content">
+          <section className="section hero">
+            <div
+              style={{
+                justifyContent: "center",
+                textAlign: "center",
+              }}
+            >
+              <motion.img
+                ref={ref}
+                src="/gambar/border-slideterakhir.png"
+                alt=""
+                style={{
+                  maxWidth: "22rem",
+                  y: smoothScroll,
+                  transition: { duration: 0.5, ease: "easeInOut" },
+                }}
+              />
+            </div>
             <motion.img
-              ref={ref}
-              src="/gambar/border-slideterakhir.png"
+              src="/gambar/floral.svg"
               alt=""
               style={{
-                maxWidth: "22rem",
-                y: smoothScroll,
-                transition: { duration: 0.5, ease: "easeInOut" },
+                maxWidth: "10rem",
+                height: "auto",
+                position: "absolute",
+                bottom: 0,
+                right: 0,
+              }}
+              animate={{
+                rotate: [0, 5, -5, 0], // Goyang kanan kiri
+                filter: [
+                  "drop-shadow(0 0 10px rgba(255, 255, 0, 0.5))",
+                  "drop-shadow(0 0 20px rgba(255, 255, 0, 1))",
+                  "drop-shadow(0 0 10px rgba(255, 255, 0, 0.5))",
+                ], // Efek glowing
+              }}
+              transition={{
+                duration: 4, // Durasi animasi
+                ease: "easeInOut",
+                repeat: Infinity, // Ulang animasi terus-menerus
+                rotate: { duration: 2, ease: "easeInOut", repeat: Infinity }, // Durasi dan pengulangan goyang
               }}
             />
-          </div>
-          <motion.img
-            src="/gambar/floral.svg"
-            alt=""
-            style={{
-              maxWidth: "10rem",
-              height: "auto",
-              position: "absolute",
-              bottom: 0,
-              right: 0,
-            }}
-            animate={{
-              rotate: [0, 5, -5, 0], // Goyang kanan kiri
-              filter: [
-                "drop-shadow(0 0 10px rgba(255, 255, 0, 0.5))",
-                "drop-shadow(0 0 20px rgba(255, 255, 0, 1))",
-                "drop-shadow(0 0 10px rgba(255, 255, 0, 0.5))",
-              ], // Efek glowing
-            }}
-            transition={{
-              duration: 4, // Durasi animasi
-              ease: "easeInOut",
-              repeat: Infinity, // Ulang animasi terus-menerus
-              rotate: { duration: 2, ease: "easeInOut", repeat: Infinity }, // Durasi dan pengulangan goyang
-            }}
-          />
 
-          <motion.img
-            src="/gambar/floral.svg"
-            alt=""
-            style={{
-              maxWidth: "10rem",
-              height: "auto",
-              position: "absolute",
-              bottom: 0,
-              left: 0,
-            }}
-            animate={{
-              rotate: [0, 5, -5, 0], // Goyang kanan kiri
-              filter: [
-                "drop-shadow(0 0 10px rgba(255, 255, 0, 0.5))",
-                "drop-shadow(0 0 20px rgba(255, 255, 0, 1))",
-                "drop-shadow(0 0 10px rgba(255, 255, 0, 0.5))",
-              ], // Efek glowing
-            }}
-            transition={{
-              duration: 4, // Durasi animasi
-              ease: "easeInOut",
-              repeat: Infinity, // Ulang animasi terus-menerus
-              rotate: { duration: 2, ease: "easeInOut", repeat: Infinity }, // Durasi dan pengulangan goyang
-            }}
-          />
-        </section>
-        <section
+            <motion.img
+              src="/gambar/floral.svg"
+              alt=""
+              style={{
+                maxWidth: "10rem",
+                height: "auto",
+                position: "absolute",
+                bottom: 0,
+                left: 0,
+              }}
+              animate={{
+                rotate: [0, 5, -5, 0], // Goyang kanan kiri
+                filter: [
+                  "drop-shadow(0 0 10px rgba(255, 255, 0, 0.5))",
+                  "drop-shadow(0 0 20px rgba(255, 255, 0, 1))",
+                  "drop-shadow(0 0 10px rgba(255, 255, 0, 0.5))",
+                ], // Efek glowing
+              }}
+              transition={{
+                duration: 4, // Durasi animasi
+                ease: "easeInOut",
+                repeat: Infinity, // Ulang animasi terus-menerus
+                rotate: { duration: 2, ease: "easeInOut", repeat: Infinity }, // Durasi dan pengulangan goyang
+              }}
+            />
+          </section>
+        </div>
+      </div> */}
+      <section
+        style={{
+          height: "100vh",
+          width: "100%",
+          background: "url('/gambar/Closing.png')",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          zIndex: 2,
+          justifyContent: "center",
+          alignItems: "center",
+          display: "flex",
+          position: "relative",
+          textAlign: "center",
+          boxSizing: "border-box",
+        }}
+      >
+        <img
+          src="/gambar/border-slideterakhir.png"
+          alt=""
           style={{
-            height: "100vh",
-            width: "100%",
-            background: "url('/gambar/Closing.png')",
-            backgroundSize: "cover",
-            backgroundPosition: "center",
-            zIndex: 2,
-            justifyContent: "center",
-            alignItems: "center",
-            display: "flex",
-            position: "relative",
-            textAlign: "center",
-            boxSizing: "border-box",
+            position: "absolute",
+            width: "270px",
+            top: 30,
+            paddingTop: "2rem",
+          }}
+        />
+
+        <div
+          className="closing-div"
+          style={{
+            paddingTop: "11rem",
           }}
         >
-          <div>
-            <p
-              style={{
-                fontSize: "0.8rem",
-                paddingLeft: "1rem",
-                paddingRight: "1rem",
-              }}
-            >
-              Menjadi suatu kebahagiaan bagi kami apabila Bapak/Ibu/Saudara/i
-              berkenan hadir dalam hari bahagia ini. Terimakasih atas segala
-              ucapan, doa, dan perhatian yang diberikan.
-            </p>
-            <motion.p
-              style={{
-                marginTop: "1rem",
-                marginBottom: "1rem",
-                minHeight: "24px",
-              }}
-            >
-              <strong>{displayedText}</strong>
-            </motion.p>
-            <p
-              style={{
-                fontSize: "2rem",
-                fontFamily: "Dancing Script",
-              }}
-            >
-              Mail & Asih
-            </p>
-          </div>
-          <Player
-            src="/gambar/butterfly.json"
-            background="transparent"
-            speed={1}
-            loop
-            autoplay
+          <p
             style={{
-              height: "100px",
-              width: "100px",
-              position: "absolute",
-              top: 0,
-              left: 0,
+              fontSize: "0.8rem",
+              paddingLeft: "1rem",
+              paddingRight: "1rem",
             }}
-          />
-          <Player
-            src="/gambar/butterfly.json"
-            background="transparent"
-            speed={1}
-            loop
-            autoplay
+          >
+            Menjadi suatu kebahagiaan bagi kami apabila Bapak/Ibu/Saudara/i
+            berkenan hadir dalam hari bahagia ini. Terimakasih atas segala
+            ucapan, doa, dan perhatian yang diberikan.
+          </p>
+          <motion.p
             style={{
-              height: "100px",
-              width: "100px",
-              position: "absolute",
-              bottom: 0,
-              right: 0,
+              marginTop: "1rem",
+              marginBottom: "1rem",
+              minHeight: "24px",
             }}
-          />
-        </section>
-      </div>
-    </div>
+          >
+            <strong>{displayedText}</strong>
+          </motion.p>
+          <p
+            style={{
+              fontSize: "2rem",
+              fontFamily: "Dancing Script",
+            }}
+          >
+            Mail & Asih
+          </p>
+        </div>
+        <Player
+          src="/gambar/butterfly.json"
+          background="transparent"
+          speed={1}
+          loop
+          autoplay
+          style={{
+            height: "100px",
+            width: "100px",
+            position: "absolute",
+            top: 0,
+            left: 0,
+          }}
+        />
+        <Player
+          src="/gambar/butterfly.json"
+          background="transparent"
+          speed={1}
+          loop
+          autoplay
+          style={{
+            height: "100px",
+            width: "100px",
+            position: "absolute",
+            bottom: 0,
+            right: 0,
+          }}
+        />
+      </section>
+    </>
   );
 };
 
